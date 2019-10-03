c6150659f46daa260f5c59334781dd95279b08f0
ALEJANDRO ESTEVE MAZA <54013692d@AD.FPMISLATA.COM>
2019-10-03 08:58:51 +0200

    index with html

--- src/index.php
+++ src/index.php
@@ -1,3 +1,10 @@
-<?php
-
-echo "Hola Mundo!";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php echo "Hola Mundo!"; ?>
+</body>
+</html>
